2c92c849dd1fe0a73638c34429559167d7ba8931
medici1 <medici1@medici1-PC>
2016-11-03 13:34:08 +0900

    B No

--- src/citest/Main.java
+++ src/citest/Main.java
@@ -3,7 +3,8 @@ package citest;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		int a;
+		a = b++;
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
